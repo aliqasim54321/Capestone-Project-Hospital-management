09ae8244d2ac87fe7d12ceb9945e5a9cedc09324
Syed Ali Qasim Khundmiri <92255662+aliqasim54321@users.noreply.github.com>
2024-10-13 15:21:45 -0400

    Fix

--- backend/routes/contact.js
+++
@@ -1,9 +0,0 @@
-// routes/contact.js
-import express from "express";
-import { submitContactForm } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/", submitContactForm);
-
-export default router;
