68c2391115ffec21f3d2da87fb1dd791a5733943
Syed Ali Qasim Khundmiri <92255662+aliqasim54321@users.noreply.github.com>
2024-10-14 15:23:15 -0400

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://capestone-project-hospital-management-mken.onrender.com";
+export const BASE_URL = "https://capestone-project-hospital-management-mken.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
